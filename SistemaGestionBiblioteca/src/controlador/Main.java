90e13c8a1e9631e3772536933526b92be8c6318a
ikasle <ikasle@DAM2-01.arangoya.local>
2024-12-10 12:06:17 +0100

    Prueba jdbc eliminar

--- SistemaGestionBiblioteca/src/controlador/Main.java
+++ SistemaGestionBiblioteca/src/controlador/Main.java
@@ -1,11 +1,15 @@
 package controlador;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
+		Scanner s = new Scanner(System.in);
+		int nota = s.nextInt();
+		
 	
 		
 	}
